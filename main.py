cc9034dd3e3c895b82b1b509739b5982c1e7a71d
Josh Jacobson <49420704+jojadev@users.noreply.github.com>
2021-02-25 21:39:54 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input("Who is your favourite Simpson household member? ")
+if name == 'Homer': 
+  print("You the man Homer!")
+elif name == 'Marge': 
+    print("Way to go mom!")
+elif name == 'Lisa': 
+    print("Eww, Lisa!")
+elif name == 'Maggie': 
+    print("What's up you cool baby?")
+else: 
+  print("I'm Bart Simpson, who the hell are you?")
+
+  #https://repl.it/@jojadev/simpsons-test
